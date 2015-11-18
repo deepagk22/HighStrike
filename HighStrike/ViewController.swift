5c3d304e25f12a6069cc2ba5666f5a8664cbdca1
deepagk22 <deepagk@rajeshs-macbook-pro-2.local>
2015-11-18 11:50:24 -0800

    Initial Commit

---
+++ HighStrike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HighStrike
+//
+//  Created by Deepa Krishnan on 11/18/15.
+//  Copyright © 2015 Deepa Krishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
